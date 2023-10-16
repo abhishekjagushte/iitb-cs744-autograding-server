2d4e09e6882d55f08dd8d11da6698efd62f44ace
Abhishek Jagushte <abhishekjagushte@gmail.com>
2023-10-16 13:31:35 +0530

    code refecator

--- gradingclient.c
+++ gradingclient.c
@@ -7,81 +7,53 @@
 #include <sys/time.h>
 
 
-int main(int argc, char *argv[]) {
-    char *fname;
-    int sockfd = 0;
+void error(char* message) {
+    printf("%s\n", message);
+    exit(1);
+}
 
-    if (argc != 7) {
-        printf("Usage: <server-IP> <server-port> <file-name> <loop num> <sleep time> <id>\n");
-        exit(1);
-    }
-
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-
-    int portno = atoi(argv[2]);
-    server = gethostbyname(argv[1]);
-
-    if (server == NULL) {
-        printf("No such host available\n");
-        exit(0);
-    }
-
-    bzero((char *)&serv_addr, sizeof(serv_addr));
-
-    serv_addr.sin_port = htons(portno);
-    serv_addr.sin_family = AF_INET;
-
+void send_files_to_server(
+    int count, int sockfd, char* fname, int sleep_time, int prog_id
+) {
+    int icount = count;
     char fbuff[10000];
-    fname = argv[3];
     int fd = open(fname, O_RDONLY);
     int fbr = read(fd, &fbuff, 10000);
-
-    int count = atoi(argv[4]);
-    int icount = count;
-    int sleep_time = atoi(argv[5]);
-    int prog_id = atoi(argv[6]);
-    int time_sum = 0;
     int succ = 0;
+    int time_sum = 0;
+
 
     // total time taken for loop
     struct timeval total_time_start;
     gettimeofday(&total_time_start, NULL);
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sockfd < 0) {
-        printf("Error in creating a socket\n");
-        exit(1);
-    }
-
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        printf("Couldn't connect!");
-        exit(1);
-    }
-
+    
     while (count--) {        
         // start time
         struct timeval start_time;
         gettimeofday(&start_time, NULL);
-        
+
         int fbw = write(sockfd, fbuff, fbr);
 
         if (fbw < 0) {
-            printf("Error in writing");
+            printf("Error in writing\n");
         } else {
-            succ++;
             char res[1000];
+                
             int resbytes = read(sockfd, &res, 1000);
-            
+
+
+            if (resbytes <= 0) {
+                printf("Error receiving response from server\n");
+                continue;
+            }
+            succ++;
+            write(STDOUT_FILENO, res, resbytes);
+
             // end time
             struct timeval end_time;
             gettimeofday(&end_time, NULL);
 
-            write(STDOUT_FILENO, res, resbytes);
-
-            int t_diff = (end_time.tv_sec*1000 + end_time.tv_usec/1000) - (start_time.tv_sec*1000 + start_time.tv_usec/1000);
+            int t_diff = (end_time.tv_sec*1000000 + end_time.tv_usec) - (start_time.tv_sec*1000000 + start_time.tv_usec);
             time_sum += t_diff;
         }
         sleep(sleep_time);
@@ -96,4 +68,49 @@ int main(int argc, char *argv[]) {
 
     close(sockfd);
     close(fd);
+}
+
+
+int main(int argc, char *argv[]) {
+    char *fname;
+    int sockfd = 0;
+
+    if (argc != 7) {
+        printf("Usage: <server-IP> <server-port> <file-name> <loop num> <sleep time> <id>\n");
+        exit(1);
+    }
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    server = gethostbyname(argv[1]);
+    int portno = atoi(argv[2]);
+    fname = argv[3];
+    int count = atoi(argv[4]);
+    int sleep_time = atoi(argv[5]);
+    int prog_id = atoi(argv[6]);
+
+    if (server == NULL) {
+        error("No such host available");
+    }
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_family = AF_INET;
+
+    int time_sum = 0;
+    int succ = 0;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        error("Error in creating a socket");
+    }
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        error("Couldn't connect!");
+    }
+
+    send_files_to_server(count, sockfd, fname, sleep_time, prog_id);
 }
